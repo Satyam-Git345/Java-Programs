04fcfcbe70199042990278d97784bf483de977ec
Satyam Shukla <satyamshukla.91000@gmail.com>
2022-12-09 00:03:20 +0530

    fourthcommit

---
+++ satyam/src/com/sistec/NewClass.java
@@ -0,0 +1,47 @@
+package com.sistec;
+import java.sql.*;
+import java.util.Scanner;
+
+
+
+public class NewClass {
+      public static void main(String[] args)
+    {
+        try
+        {
+            Class.forName("oracle.jdbc.OracleDriver");
+            System.out.println("Driver loaded ");
+            Connection con = DriverManager.getConnection("jdbc:oracle:thin:@//sir:1521/xe","satyam", "12345");
+            Statement st = con.createStatement();
+            Scanner kb = new Scanner(System.in);
+            for(int i=1; i<=2 ; i++)
+            {
+                System.out.println("Enter the id , name ,sal = ");
+                int id = kb.nextInt();
+                String name = kb.next();
+                double sal = kb.nextDouble();
+                String qry ="insert into employee values ("+id+",'"+name+"',"+sal+")";
+                System.out.println("qry = "+qry);
+            int c = st.executeUpdate(qry);
+            if(c==0)
+                System.out.println("Not row updated");
+            else
+                System.out.println("No of row Updated = "+ c);
+            } 
+            con.close();
+        }
+        catch(ClassNotFoundException e)
+        {
+            System.out.println("error ="+e);
+        }
+        catch(SQLException e)
+        {
+            System.out.println("ERROR = "+e);
+        }
+    }
+    
+}
+
+
+ 
+ 
